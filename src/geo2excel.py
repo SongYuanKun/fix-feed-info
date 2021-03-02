1f7f8d4d214269fe3f217971012adc3123225445
songyuankun <songyuankun@huohua.cn>
2021-03-02 14:24:14 +0800

    init

--- src/geo2excel.py
+++ src/geo2excel.py
@@ -4,16 +4,17 @@ import json
 import requests
 
 if __name__ == '__main__':
-    response = requests.get("http://127.0.0.1:8080/geo/child/info/v2?pid=54489")
+    response = requests.get("http://10.251.57.189:8080/geo/child/info/v2?pid=54489")
     p = json.loads(response.content)
-    with open('国内所有城市.csv', 'w', newline='') as csv_file:
+    with open('国内所有省份+城市.csv', 'w', newline='') as csv_file:
         sp = csv.writer(csv_file)
-        sp.writerow(["id", '名称'])
+        sp.writerow(["省份id", '省份名称', "城市id", '城市名称'])
         for info in p['data']:
             if info['municipality'] == 1:
-                sp.writerow([info['id'], info['name']])
+                sp.writerow([info['id'], info['name'], '', ''])
             else:
-                response = requests.get("http://127.0.0.1:8080/geo/child/info/v2?level=2&pid="+str(info['id']))
+                sp.writerow([info['id'], info['name'], '', ''])
+                response = requests.get("http://10.251.57.189:8080/geo/child/info/v2?level=2&pid=" + str(info['id']))
                 c = json.loads(response.content)
                 for city in c['data']:
-                    sp.writerow([city['id'], city['name']])
+                    sp.writerow(['', '', city['id'], city['name']])
