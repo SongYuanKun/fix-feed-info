990b31db3822d06e23601d3940a1f22bf852dceb
songyuankun <songyuankun@huohua.cn>
2021-06-17 21:42:53 +0800

    ip

--- src/geo2excel.py
+++ src/geo2excel.py
@@ -4,7 +4,10 @@ import json
 import requests
 
 if __name__ == '__main__':
-    response = requests.get("http://10.251.57.189:8080/geo/child/info/v2?pid=54489")
+    # QA系统
+    host = 'http://10.251.127.40:8080'
+
+    response = requests.get(host + "/geo/child/info/v2?pid=54489")
     p = json.loads(response.content)
     with open('国内所有省份+城市+区.csv', 'w', newline='') as csv_file:
         sp = csv.writer(csv_file)
@@ -12,18 +15,17 @@ if __name__ == '__main__':
         for info in p['data']:
             if info['municipality'] == 1:
                 sp.writerow([info['id'], info['name'], '', '', '', ''])
-                response = requests.get("http://10.251.57.189:8080/geo/child/info/v2?level=2&pid=" + str(info['id']))
+                response = requests.get(host + "/geo/child/info/v2?level=2&pid=" + str(info['id']))
                 c = json.loads(response.content)
                 for city in c['data']:
                     sp.writerow(['', '', '', '', city['id'], city['name'], ])
             else:
                 sp.writerow([info['id'], info['name'], '', '', '', ''])
-                response = requests.get("http://10.251.57.189:8080/geo/child/info/v2?level=2&pid=" + str(info['id']))
+                response = requests.get(host + "/geo/child/info/v2?level=2&pid=" + str(info['id']))
                 c = json.loads(response.content)
                 for city in c['data']:
                     sp.writerow(['', '', city['id'], city['name'], '', ''])
-                    response = requests.get(
-                        "http://10.251.57.189:8080/geo/child/info/v2?level=3&pid=" + str(city['id']))
+                    response = requests.get(host + "/geo/child/info/v2?level=3&pid=" + str(city['id']))
                     d = json.loads(response.content)
                     for ac in d['data']:
                         sp.writerow(['', '', '', '', ac['id'], ac['name']])
